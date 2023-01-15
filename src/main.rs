7ee1a0fc6adfde1ed1081df367dac577f92fb8dc
Vamanu Petru <petrisor_vmy@yahoo.com>
2023-01-15 19:21:30 +0200

    continuous

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use chrono::{DateTime, Local, NaiveDateTime, Utc};
 use clap::Parser;
-use std::env;
-use chrono::{DateTime, Utc, NaiveDateTime, Local};
+use std::collections::HashSet;
+use std::thread::sleep;
+use std::time::Duration;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -10,33 +12,32 @@ struct Args {
 
     #[arg(long, default_value_t = String::from("hot"))]
     sort: String,
-    
+    // #[arg(long, default_value_t = false)]
+    // : bool,
 }
 
 use serde_derive::Deserialize;
 #[derive(Deserialize)]
-struct RedditInfo{
-    title : String,
-    author : String,
-    permalink : String,
-    created_utc : f32,
+struct RedditInfo {
+    title: String,
+    author: String,
+    permalink: String,
+    created_utc: f32,
 }
 
 #[derive(Deserialize)]
-struct RedditPost
-{
+struct RedditPost {
     // kind: String,
     data: RedditInfo,
 }
 
-
 #[derive(Deserialize)]
-struct RedditPostWrapper{
-    children : Vec<RedditPost>,
+struct RedditPostWrapper {
+    children: Vec<RedditPost>,
 }
 
 #[derive(Deserialize)]
-struct RedditResponse{
+struct RedditResponse {
     data: RedditPostWrapper,
 }
 
@@ -48,28 +49,45 @@ fn get_api(url: &str) -> Result<String, ureq::Error> {
     Ok(body)
 }
 
-fn get_local_time(timestamp: f32) -> String
-{
-    let datetime: DateTime<Utc> = DateTime::from_utc(NaiveDateTime::from_timestamp(timestamp as i64, 0), Utc);
+fn get_local_time(timestamp: f32) -> String {
+    let datetime: DateTime<Utc> =
+        DateTime::from_utc(NaiveDateTime::from_timestamp(timestamp as i64, 0), Utc);
     let local_datetime = datetime.with_timezone(&Local);
     local_datetime.format("%d/%m/%Y %H:%M").to_string()
 }
 fn main() {
     let args = Args::parse();
-    let url = format!("https://www.reddit.com/r/{}/{}.json", args.subreddit, args.sort);
-    let response_data = get_api(url.as_str());
-    let mut json_data = String::new();
-    match response_data
-    {
-        Ok(response) => { json_data = response},
-        Err(_) => { /* some kind of io/transport error */ }
-    };  
+    let url = format!(
+        "https://www.reddit.com/r/{}/{}.json",
+        args.subreddit, args.sort
+    );
 
+    let seconds = 2;
 
-    
-    let json_string : RedditResponse = serde_json::from_str(&json_data).unwrap();
-    
-    for item in json_string.data.children.into_iter() {
-        println!("{}, {} \n{} \nhttps://www.reddit.com{}\n\n", item.data.title, item.data.author, get_local_time(item.data.created_utc), item.data.permalink);
+    let mut posts_set = HashSet::<String>::new();
+
+    loop {
+        let response_data = get_api(url.as_str());
+        let mut json_data = String::new();
+        match response_data {
+            Ok(response) => json_data = response,
+            Err(_) => { /* some kind of io/transport error */ }
+        };
+
+        let json_string: RedditResponse = serde_json::from_str(&json_data).unwrap();
+
+        for item in json_string.data.children.into_iter() {
+            if !posts_set.contains(&item.data.permalink) {
+                posts_set.insert(item.data.permalink.clone());
+                println!(
+                    "{}, {} \n{} \nhttps://www.reddit.com{}\n\n",
+                    item.data.title,
+                    item.data.author,
+                    get_local_time(item.data.created_utc),
+                    item.data.permalink
+                );
+            }
+        }
+        sleep(Duration::from_secs(seconds));
     }
 }
